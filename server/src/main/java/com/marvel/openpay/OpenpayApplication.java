f362d4bb53c193c9b77cf01fc68be105ed0ba1c4
Fer Rouco <fernando.rouco@fiserv.com>
2023-12-02 17:50:53 -0300

    Server project

---
+++ server/src/main/java/com/marvel/openpay/OpenpayApplication.java
@@ -0,0 +1,13 @@
+package com.marvel.openpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenpayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenpayApplication.class, args);
+	}
+
+}
