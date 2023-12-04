57f780e9222cc1fefdec2fcbe084075864d86d28
Fer Rouco <fernando.rouco@fiserv.com>
2023-12-04 15:11:56 -0300

    User name in app component

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, Inject } from '@angular/core';
+import { Component, Inject, WritableSignal, signal, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router, RouterOutlet } from '@angular/router';
 import { FrameworkModule } from './framework/framework.module';
@@ -12,13 +12,21 @@ import { SessionService } from './services/session.service';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  
+export class AppComponent implements OnInit {
+  public userName: WritableSignal<string> = signal('');
+
   constructor(protected sessionService: SessionService, @Inject(Router) protected router: Router) {}
+  ngOnInit(): void {
+    this.userName.set(this.getCurrentUser());
+  }
 
   doLogout(): void {
     this.sessionService.logOut();
     this.router.navigateByUrl('/login');
+  }
+
+  getCurrentUser(): string {
+    return this.sessionService.getCurrentUserName();
   }
 
   isAuthenticatedUser(): boolean {
