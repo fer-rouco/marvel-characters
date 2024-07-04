c0a29143410898fcf811f89451971089d7253bf0
Fernando Nicolás Rouco <fernando.rouco.contractor@openpay.mx>
2024-07-04 09:08:14 -0300

    Changing anchor href to routerLink

--- client/src/app/app.config.ts
+++ client/src/app/app.config.ts
@@ -1,8 +1,12 @@
+import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes)]
+  providers: [
+    provideRouter(routes, withHashLocation()),  // Enable hash location strategy
+    { provide: LocationStrategy, useClass: HashLocationStrategy }
+  ]
 };
--- client/src/app/framework/framework.module.ts
+++ client/src/app/framework/framework.module.ts
@@ -1,15 +1,16 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { PanelComponent } from './containers/panel/panel.component';
-import { InputFieldComponent } from './controls/fields/input-field/input-field.component';
+import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { FieldComponent } from './controls/fields/field/field.component';
-import { NumericFieldComponent } from './controls/fields/numeric-field/numeric-field.component';
+import { RouterLink } from '@angular/router';
+import { PanelComponent } from './containers/panel/panel.component';
 import { BaseFieldComponent } from './controls/base-field.component';
 import { CustomButtonComponent } from './controls/button/button.component';
-import { TableComponent } from './controls/table/table.component';
-import { TextFieldComponent } from './controls/fields/text-field/text-field.component';
+import { FieldComponent } from './controls/fields/field/field.component';
+import { InputFieldComponent } from './controls/fields/input-field/input-field.component';
+import { NumericFieldComponent } from './controls/fields/numeric-field/numeric-field.component';
 import { PasswordFieldComponent } from './controls/fields/password-field/password-field.component';
+import { TextFieldComponent } from './controls/fields/text-field/text-field.component';
+import { TableComponent } from './controls/table/table.component';
 @NgModule({
   declarations: [
     PanelComponent,
@@ -31,6 +32,6 @@ import { PasswordFieldComponent } from './controls/fields/password-field/passwor
     CustomButtonComponent,
     TableComponent
   ],
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, RouterLink],
 })
 export class FrameworkModule {}
